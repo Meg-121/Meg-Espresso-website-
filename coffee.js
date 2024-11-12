f3416d12cd2dc412712d96ce1f94da43ea11cfdb
Meg-121 <bp16102011@gmail.com>
2024-11-12 17:43:40 +0000

    Add files via upload

---
+++ coffee.js
@@ -0,0 +1,12 @@
+
+// var hello=document.getElementById("Megaform");
+// hello.onsubmit=function(event) {
+//     event.preventDefault();
+//  alert("Form submiited, you will recieve an email shortly.");
+//   }
+
+function showAlert(event) {
+
+    event.preventDefault(); // Prevents actual form submission
+
+    alert("Form submitted");}
